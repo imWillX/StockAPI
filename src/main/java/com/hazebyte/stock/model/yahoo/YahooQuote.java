98807e5cb87779b72f18201515ae5fe91812f7b1
imWillX <imWillX@users.noreply.github.com>
2021-02-09 21:17:41 -0800

    fix: return of YahooQuote

--- src/main/java/com/hazebyte/stock/model/yahoo/YahooQuote.java
+++ src/main/java/com/hazebyte/stock/model/yahoo/YahooQuote.java
@@ -1,12 +1,12 @@
 package com.hazebyte.stock.model.yahoo;
 
 import com.hazebyte.stock.model.Quote;
-import com.hazebyte.stock.model.QuoteResult;
 
 public class YahooQuote implements Quote {
     private YahooQuoteResponse quoteResponse;
 
-    public QuoteResult[] getQuotes() {
+    @Override
+    public YahooQuoteResult[] getQuotes() {
         return quoteResponse.result;
     }
 
